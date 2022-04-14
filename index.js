c2e2dc77097e9f5e3ef7ac4b4b8cd607d62efbe5
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-04-14 15:59:53 -0700

    adding auth and passport

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const res = require('express/lib/response')
+require('./services/passport')
+const authRoutes = require('./routes/authRoutes')
+
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' })
-})
+authRoutes(app)
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT)
